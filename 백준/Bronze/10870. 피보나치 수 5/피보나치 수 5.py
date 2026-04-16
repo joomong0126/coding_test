3ae5b9d6edf9578ef19de88f0b38c240d70b54ad
2__ga_1 <77429760+joomong0126@users.noreply.github.com>
2026-04-16 20:16:53 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,10 +1,12 @@
-def fibonacci(n):
+def fib(n):
+    # 기저 조건 1: n == 0이면 0 반환
     if n == 0:
         return 0
+    # 기저 조건 2: n == 1이면 1 반환
     if n == 1:
         return 1
-    
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    # 재귀 호출: fib(n-1) + fib(n-2)
+    return fib(n-1) + fib(n-2)
 
 n = int(input())
-print(fibonacci(n))
+print(fib(n))
