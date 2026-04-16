06dd775d19bc448585bf4b7777c8af05e3935a8d
2__ga_1 <77429760+joomong0126@users.noreply.github.com>
2026-04-16 20:17:12 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,6 +1,8 @@
 def factorial(n):
-    if n <= 1:
+    # 기저 조건을 작성하세요 (n == 0이면 1 반환)
+    if n == 0:
         return 1
+    # 재귀 호출을 작성하세요 (n * factorial(n-1))
     return n * factorial(n-1)
 
 n = int(input())
